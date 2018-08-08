0ec62eee06fc9dd63b241100585f37adeebd9f2f
Ying Linhan <yinglinhan1987@gmail.com>
2018-08-08 21:20:44 +0800

    test

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ module.exports ={
     output:{
         path:__dirname + '/out',
         filename:'[name].bundle.js',
-        publicPath:'./out'
+        publicPath:'/out/static'
     },
     mode:'development',
     module:{
@@ -14,25 +14,17 @@ module.exports ={
                  * 末尾 \?.* 匹配带 ? 资源路径
                  * 我们引入的第三方 css 字体样式对字体的引用路径中可能带查询字符串的版本信息
                  */
-                test: /\.(woff2|woff|eot|ttf|otf)(\?.*)?$/,
-                /**
-                 * url-loader
-                 * 会配合 webpack 对资源引入路径进行复写，如将 css 提取成独立文件，可能出现 404 错误可查看 提取 js 中的 css 部分解决
-                 * 会以 webpack 的输出路径为基本路径，以 name 配置进行具体输出
-                 * limit 单位为 byte，小于这个大小的文件会编译为 base64 写进 js 或 html
-                 */
+                test: /.jpg|gif|png|svg|woff2|woff|eot|ttf|otf$/,
                 loader: 'url-loader',
                 options: {
-                  limit: 10000,
-                  name: 'static/fonts/[name].[hash:7].[ext]',
+                  limit: 10000
                 }
               },
             {
-                test: /\.less$/,
+                test: /.less$/,
                 use: [
                   'style-loader', 
                   'css-loader', 
-                  'postcss-loader', 
                   'less-loader'
                 ]
               }
